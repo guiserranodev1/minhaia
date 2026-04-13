4bd4477ccc1ce0657a278bd3b31d756ada3acf11
Guilherme Meireles Serrano <1292416188@ulife.com.br>
2026-04-13 20:41:07 -0300

    MinhaIA - Base

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+
+
+genai.configure(api_key="AIzaSyCNP5IcXq97QLuSq3RLdAaquqsGjMlZ0AU")
+
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+        user_input = request.json["message"]
+
+        response = model.generate_content(user_input)
+
+        return jsonify({
+            "reply": response.text
+        })
+
+    except Exception as e:
+        return jsonify({
+            "reply": f"Erro: {str(e)}"
+        })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
